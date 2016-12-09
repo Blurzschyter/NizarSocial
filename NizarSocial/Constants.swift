85896890e7616e8ae5879a70e5d3340397eb9ffa
Blurzschyter <sharulnizar.razali@gmail.com>
2016-12-09 15:21:43 +0800

    Sign in ui custom complete

---
+++ NizarSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  NizarSocial
+//
+//  Created by NM on 09/12/2016.
+//  Copyright © 2016 nizar. All rights reserved.
+//
+
+import UIKit //UIkit also contains Foundation
+
+let SHADOW_GREY: CGFloat = 120.0 / 255.0
