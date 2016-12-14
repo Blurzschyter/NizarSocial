f06c9decd0c3224a25a772fa5a262ef4e88d41d9
Blurzschyter <sharulnizar.razali@gmail.com>
2016-12-14 16:49:58 +0800

    Complete integration between Firebase, Facebook, and IOS

--- NizarSocial/SignInVC.swift
+++ NizarSocial/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -14,12 +17,69 @@ class SignInVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func fbLoginPressed(_ sender: AnyObject) {
+        
+        FBSDKLoginManager().logIn(withReadPermissions: ["email"], from: self) { (result, err) in
+            
+//            if err != nil {
+//                print("Custom FB login failed.")
+//                return
+//            } else {
+//                print("SUCCESS LOGIN")
+//            }
+//            
+//            print(result?.token.tokenString)
+            
+            if err != nil {
+                print("NIZAR : Unable to authenticate with facebook - \(err)")
+            } else if result?.isCancelled == true {
+                print("NIZAR : User cancelled facebook authentication")
+            } else {
+                print("NIZAR : SUCCESSFULLY authenticate.")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+            
+        }
     }
-
-
+    
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil {
+                print("NIZAR : Unable to authenticate with firebase - \(error)")
+            } else {
+                print("NIZAR : SUCCESS firebase authentication.")
+            }
+        })
+    }
+    
+    
+//    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+//        print("Did logout facebook.")
+//    }
+    
+//    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+//        
+//        if error != nil {
+//            print(error)
+//            return
+//        }
+//        
+//        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields" : "id, name, email"]).start { (connection, result, err) in
+//            
+//            if err != nil {
+//                print("Failed to start graph request: ", err)
+//                return
+//            } else {
+//                print("SUCCESS START GRAPH REQUEST")
+//            }
+//            
+//            print(result)
+//        }
+//    }
 }
 
