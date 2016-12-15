0ee229831212086ce717ac24d59a69c78d9c574e
Blurzschyter <sharulnizar.razali@gmail.com>
2016-12-15 11:51:45 +0800

    complete email auth

--- NizarSocial/SignInVC.swift
+++ NizarSocial/SignInVC.swift
@@ -12,6 +12,10 @@ import FBSDKLoginKit
 import Firebase
 
 class SignInVC: UIViewController {
+    
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +25,6 @@ class SignInVC: UIViewController {
     @IBAction func fbLoginPressed(_ sender: AnyObject) {
         
         FBSDKLoginManager().logIn(withReadPermissions: ["email"], from: self) { (result, err) in
-            
-//            if err != nil {
-//                print("Custom FB login failed.")
-//                return
-//            } else {
-//                print("SUCCESS LOGIN")
-//            }
-//            
-//            print(result?.token.tokenString)
             
             if err != nil {
                 print("NIZAR : Unable to authenticate with facebook - \(err)")
@@ -45,7 +40,6 @@ class SignInVC: UIViewController {
         }
     }
     
-    
     func firebaseAuth(_ credential: FIRAuthCredential) {
         FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
             
@@ -57,29 +51,28 @@ class SignInVC: UIViewController {
         })
     }
     
+    @IBAction func signInPressed(_ sender: AnyObject) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil { //user registered in the system.
+                    print("NIZAR : CURRENT USER SUSCESSFULLY AUTHENTICATE WITH FIREBASE.")
+                } else { //user not registered. auto registered.
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("NIZAR : Unable to authenticate new user using Firebase's Email")
+                        } else {
+                            print("NIZAR : Successfully authenticate & create user using Firebase's email")
+                        }
+                    })
+                }
+                
+            })
+            
+        }
+        
+    }
     
-//    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-//        print("Did logout facebook.")
-//    }
     
-//    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-//        
-//        if error != nil {
-//            print(error)
-//            return
-//        }
-//        
-//        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields" : "id, name, email"]).start { (connection, result, err) in
-//            
-//            if err != nil {
-//                print("Failed to start graph request: ", err)
-//                return
-//            } else {
-//                print("SUCCESS START GRAPH REQUEST")
-//            }
-//            
-//            print(result)
-//        }
-//    }
 }
-
