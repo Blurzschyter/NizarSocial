0469f76941067a2afc368d0de8fc7f8e354a9c32
Blurzschyter <sharulnizar.razali@gmail.com>
2016-12-17 21:13:47 +0800

    Feed UI part 1 complete

---
+++ NizarSocial/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  NizarSocial
+//
+//  Created by NM on 16/12/2016.
+//  Copyright © 2016 nizar. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
